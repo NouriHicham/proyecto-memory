abc9899ad0f845e0afce2ab7f0bb8026e14e2ad0
NouriHicham <nourichamidhicham@fpllefia.com>
2025-04-23 17:48:38 +0200

    segunda prueba para desplegar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,45 +3,57 @@
 import { Memoria } from "@/assets/memoria";
 import { useEffect, useState } from "react";
 
+declare global {
+  interface Window {
+    rotarCarta: (key: number) => void;
+  }
+}
+
 export default function Home() {
   const memoria = new Memoria();
   const [clicks, setClicks] = useState(0);
   const [tiempoAgotado, setTiempoAgotado] = useState(false);
   const [contador, setContador] = useState(30);
 
-  useEffect(() => {
-    memoria.shuffle();
-    memoria.dibujaMemoria("memoria-container");
+  useEffect(
+    () => {
+      memoria.shuffle();
+      memoria.dibujaMemoria("memoria-container");
 
-    (window as any).rotarCarta = (key: number) => {
-      memoria.rotarCarta(key);
-      setClicks(memoria.clicks);
-    };
-  }, [memoria]);
+      window.rotarCarta = (key: number) => {
+        memoria.rotarCarta(key);
+        setClicks(memoria.clicks);
+      };
+    }, [/* memoria */]); // eslint-disable-line react-hooks/exhaustive-deps
 
   //tienes 20 segundos para terminar el juego
   useEffect(() => {
     const intervalId = setInterval(() => {
       if (!memoria.win) {
-        setContador(contador - 1); // decrementa el contador cada segundo
+        setContador(contador - 1);
         if (contador === 0) {
           setTiempoAgotado(true);
         }
       }
-    }, 1000); // ejecuta cada segundo
+    }, 1000);
     return () => clearInterval(intervalId);
   }, [memoria.win, contador]);
 
   return (
     <>
-    <div className="m-5">
-      <h1>Juego de Memoria</h1>
-      <p>Clicks: {clicks}</p>
-      <p>Tiempo restante: {contador} segundos</p>
-      <div id="memoria-container" className={`grid grid-cols-4 gap-2 w-[350px] ${tiempoAgotado ? 'text-red-500 text-2xl' : ''}`}>
-        {tiempoAgotado ? 'Tiempo agotado' : ''}
+      <div className="m-5">
+        <h1>Juego de Memoria</h1>
+        <p>Clicks: {clicks}</p>
+        <p>Tiempo restante: {contador} segundos</p>
+        <div
+          id="memoria-container"
+          className={`grid grid-cols-4 gap-2 w-[350px] ${
+            tiempoAgotado ? "text-red-500 text-2xl" : ""
+          }`}
+        >
+          {tiempoAgotado ? "Tiempo agotado" : ""}
+        </div>
       </div>
-    </div>
     </>
   );
 }
